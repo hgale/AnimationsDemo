271cff03a77da827a77f2363f4972ac60cda7d92
Harry Gale <harry.n.gale@gmail.com>
2018-01-17 18:17:13 -0800

    Parallel animation example

--- App.js
+++ App.js
@@ -3,72 +3,130 @@
  */
 
 import React, { Component } from 'react';
-import { View, Animated, Button, StyleSheet } from 'react-native';
+import { Dimensions, Animated, View, TouchableHighlight, Text, StyleSheet } from 'react-native';
 
-/* Can Animate any of the following:
-*  Animated.Image
-*  Animated.ScrollView
-*  Animated.Text
-*  Animated.View
-*/
+ /* Can Animate any of the following:
+ *  Animated.Image
+ *  Animated.ScrollView
+ *  Animated.Text
+ *  Animated.View
+ */
+
+ const { height } = Dimensions.get('window');
+
 
 export default class App extends Component<{}> {
 
-    animatedMargin = new Animated.Value(0);
-    animatedWidth = new Animated.Value(50);
-    animatedHeight = new Animated.Value(50);
+  state = {
+   loaded: false,
+ }
 
-    animateMargin = () => {
-      Animated.timing(
-        this.animatedMargin,
-        {
-          toValue: this.animatedMargin._value + 200,
-          duration: 1000,
-        },
-      ).start();
-    }
+ componentDidMount() {
+   this.triggerAnimations();
+ }
 
-    animateSize = () => {
-      Animated.timing(
-        this.animatedWidth,
-        {
-          toValue: this.animatedWidth._value + 50,
-          duration: 500,
-        },
-      ).start();
-      Animated.timing(
-        this.animatedHeight,
-        {
-          toValue: this.animatedHeight._value + 50,
-          duration: 500,
-        },
-      ).start();
-    }
+ componentWillUnmount() {
+   clearTimeout(this.timeOut);
+ }
 
-    render() {
-      return (
-        <View style={styles.container}>
-          <Button title="Animate Margin" onPress={this.animateMargin} />
-          <Button title="Animate Size" onPress={this.animateSize} />
+ animatedWelcome = new Animated.Value(0);
+ animatedButton = new Animated.Value(0);
 
-          <Animated.View
-            style={[
-              styles.block,
-              { marginTop: this.animatedMargin },
-              { width: this.animatedWidth, height: this.animatedHeight },
-            ]}
-          />
-        </View>
-      );
-    }
+ triggerAnimations = () => {
+   this.timeOut = setTimeout(() => {
+     this.setState({ loaded: true });
+     this.animateAll();
+   }, 1300);
+ }
+
+ animateAll = () => {
+   Animated.parallel([
+     Animated.timing(
+       this.animatedWelcome,
+       {
+         toValue: 1,
+         duration: 740,
+       },
+     ),
+     Animated.timing(
+       this.animatedButton,
+       {
+         toValue: 1,
+         duration: 1700,
+       },
+     ),
+   ]).start();
+ }
+
+ render() {
+   console.log('height:', height);
+   const buttonMargin = this.animatedButton.interpolate({
+     inputRange: [0, 1],
+     outputRange: [667, 250],
+   });
+
+   const welcomeMargin = this.animatedWelcome.interpolate({
+     inputRange: [0, 1],
+     outputRange: [-100, 230],
+   });
+
+   return (
+     <View style={styles.container}>
+       { !this.state.loaded && (
+         <View style={styles.loadingContainer}>
+           <Text>Loading...</Text>
+         </View>)
+       }
+       { this.state.loaded && (
+         <View>
+           <Animated.View style={[styles.welcomeContainer, { marginTop: welcomeMargin }]}>
+             <Text style={styles.welcomeText}>Welcome</Text>
+           </Animated.View>
+           <Animated.View style={[styles.buttonContainer, { marginTop: buttonMargin }]}>
+             <TouchableHighlight underlayColor='#dddddd' onPress={console.log} style={styles.button}>
+               <Text style={styles.buttonText}>CLICK HERE TO GET STARTED</Text>
+             </TouchableHighlight>
+           </Animated.View>
+         </View>)
+       }
+     </View>
+   );
+ }
 }
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 50,
-    alignItems: 'center',
+    flex: 1
   },
-  block: {
-    backgroundColor: '#FF9900',
-  },
+  loadingContainer: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center',
+   },
+   welcomeContainer: {
+     position: 'absolute',
+     justifyContent: 'center',
+     alignItems: 'center',
+     width: '100%',
+   },
+   welcomeText: {
+     fontSize: 20,
+     fontWeight: '300',
+     textAlign: 'center',
+   },
+   button: {
+     height: 60,
+     backgroundColor: '#cccccc',
+     justifyContent: 'center',
+     alignItems: 'center',
+   },
+   buttonText: {
+     fontWeight: '200',
+   },
+   buttonContainer: {
+     position: 'absolute',
+     width: '100%',
+     padding: 10,
+   },
 })
